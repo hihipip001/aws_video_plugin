4d09487159d7bef228880057a61793126475dd09
HSIEH <hihipip@ibosscom.com>
2020-10-05 08:10:35 +0800

    1.bug fix2

--- lib/aws_video_plugin.dart
+++ lib/aws_video_plugin.dart
@@ -472,25 +472,15 @@ class AWSPlayerController {
     _methodChannel.invokeMethod("dispose", {'close': true});
   }
 
-}
-
-
-
-/*
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-class AwsVideoPlugin {
-  static const MethodChannel _channel =
-      const MethodChannel('aws_video_plugin');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  void reStart(String url) async {
+    await this._initialize(
+      url,
+    );
   }
+
 }
-*/
+
+
 
 
 
